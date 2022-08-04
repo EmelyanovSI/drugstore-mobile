29801b93086f6cadcaf381ca61c4c92f9e07108f
EmelyanovSI <xemelyanov@gmail.com>
2022-08-04 05:16:45 +0300

    api routes updated

--- app/src/main/java/by/gsu/drugstore/rest/API.kt
+++ app/src/main/java/by/gsu/drugstore/rest/API.kt
@@ -7,23 +7,23 @@ import retrofit2.http.Query
 
 interface API {
 
-    @GET("api/get_all_drugs.php")
+    @GET("/get_all_drugs.php")
     fun getAllDrugs(): Call<DrugsResponse>
 
-    @GET("api/get_drugs.php")
+    @GET("/get_drugs.php")
     fun getDrugs(@Query("table_name") tableName: String): Call<DrugsResponse>
 
-    @GET("api/search_drugs.php")
+    @GET("/search_drugs.php")
     fun searchDrugs(@Query("name") name: String): Call<DrugsResponse>
 
-    @GET("api/add_drug.php")
+    @GET("/add_drug.php")
     fun addDrug(
         @Query("name") name: String,
         @Query("composition") composition: String,
         @Query("country") country: String
     ): Call<DrugsResponse>
 
-    @GET("api/remove_drug.php")
+    @GET("/remove_drug.php")
     fun removeDrug(
         @Query("id") id: Int,
         @Query("country") country: String
